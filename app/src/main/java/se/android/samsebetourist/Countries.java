49c9f627923edef5c0f00229dc3bbb71a1e36c80
Sergey Emelyanov <sergey.emelyanov@ctconsult.ru>
2019-09-23 19:39:55 +0300

    first commit

---
+++ app/src/main/java/se/android/samsebetourist/Countries.java
@@ -0,0 +1,17 @@
+package se.android.samsebetourist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Countries extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countries);
+    }
+}
