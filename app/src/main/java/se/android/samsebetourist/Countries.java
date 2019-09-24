c8890239d6ee53bf4020101e70834c6abb62ae11
Sergey Emelyanov <sergey.emelyanov@ctconsult.ru>
2019-09-24 19:06:58 +0300

    added RecycleView as TextView

--- app/src/main/java/se/android/samsebetourist/Countries.java
+++ app/src/main/java/se/android/samsebetourist/Countries.java
@@ -1,17 +1,55 @@
 package se.android.samsebetourist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Countries extends AppCompatActivity {
 
-
+        List<String> listik = new ArrayList<>();
+        RecyclerView recyclerCountries;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countries);
+
+
+        fillCountriesTank();
+        recyclerCountries = findViewById(R.id.recycler_countries);
+        recyclerCountries.setLayoutManager(new LinearLayoutManager(this));
+
+        // создаем адаптер
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_countries);
+        DataAdapter adapter = new DataAdapter(this, listik);
+        // устанавливаем для списка адаптер
+        recyclerView.setAdapter(adapter);
+
+
     }
+
+
+    private void fillCountriesTank(){
+        listik.add("Russia");
+        listik.add("Germany");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+        listik.add("Spanish");
+    }
+
+
+
 }
---
+++ app/src/main/java/se/android/samsebetourist/DataAdapter.java
@@ -0,0 +1,47 @@
+package se.android.samsebetourist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+
+public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+
+    private LayoutInflater inflater;
+    private List<String> countries;
+
+    DataAdapter(Context context, List<String> countries) {
+        this.countries = countries;
+        this.inflater = LayoutInflater.from(context);
+    }
+    @Override
+    public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = inflater.inflate(R.layout.activity_list_countries, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DataAdapter.ViewHolder holder, int position) {
+        String country = countries.get(position);
+        holder.country.setText(country);
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView country;
+        ViewHolder(View view){
+            super(view);
+            country = (TextView) view.findViewById(R.id.country);
+        }
+    }
+}
